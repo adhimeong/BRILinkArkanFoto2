981f361d92384377601478343d4e97e966613109
adhimeong <adhimeong@gmail.com>
2018-05-14 11:34:22 +0700

    update fitur scan id card

--- app/src/main/java/id/sch/smkn13bdg/adhi/brilinkarkanfoto/InputIdCardActivity.java
+++ app/src/main/java/id/sch/smkn13bdg/adhi/brilinkarkanfoto/InputIdCardActivity.java
@@ -1,29 +1,68 @@
 package id.sch.smkn13bdg.adhi.brilinkarkanfoto;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class InputIdCardActivity extends AppCompatActivity {
+
+    public Button loginbtn;
+    public EditText result;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_input_id_card);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+
+        loginbtn = (Button) findViewById(R.id.btnlogin);
+        result = (EditText) findViewById(R.id.editidkartu);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_scan);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), ScanCardActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                result.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        result.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
+
     }
 
 }
